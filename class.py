1ced9bb3ac836ce0a29a5bbaef9278d3a2ca373e
Dimaximo <d.maximoalbuquerque@gmail.com>
2023-03-23 00:59:14 -0300

    first commit

---
+++ class.py
@@ -0,0 +1,27 @@
+
+#coercion
+print(int('22') + 1)
+print(float('22.3') + 1)
+
+#math symbols
+
+adicao = 10 + 10
+print(adicao)
+
+subtracao = 10-5
+print(subtracao)
+
+multiplicacao = 10*10
+print(multiplicacao)
+
+divisao = 10/2.2
+print(divisao)
+
+divisao_inteira = 10//2.5
+print(divisao_inteira)
+
+exponencial = 2**10
+print(exponencial)
+
+modulo = 25 % 5 #resto da divisão
+print("modulo", modulo)
---
+++ exercise.py
@@ -0,0 +1,8 @@
+name= "Diego"
+weight = 120
+height = 1.81
+imc = weight/(height**2)
+
+print(name, "tem", height, "de altura")
+print("pesa", weight, "kg")
+print(name, "tem o imc de:", imc)
